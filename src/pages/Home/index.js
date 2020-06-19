ff9312a68356f4391719537a310dbf089c8f397f
ArjDev69 <bruno96araujo@gmail.com>
2020-06-19 14:43:52 -0300

    call api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,80 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
 
+import api from '../../services/api';
+
+import { formatPrice } from '../../utils';
+
 const img =
   'https://images.unsplash.com/photo-1514723432099-9f0d4906e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80';
 
-function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img src={img} alt="imagem" />
-        <strong>Tenis</strong>
-        <span>R$ 120,88</span>
+export default class Home extends Component {
+  state = {
+    product: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+  async componentDidMount() {
+    const response = await api.get('/products');
 
-      <li>
-        <img src={img} alt="imagem" />
-        <strong>Tenis</strong>
-        <span>R$ 120,88</span>
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+    this.setState({
+      product: data,
+    });
+  }
 
-      <li>
-        <img src={img} alt="imagem" />
-        <strong>Tenis</strong>
-        <span>R$ 120,88</span>
+  render() {
+    const { product } = this.state;
+    return (
+      <ProductList>
+        {product.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src={img} alt="imagem" />
-        <strong>Tenis</strong>
-        <span>R$ 120,88</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src={img} alt="imagem" />
-        <strong>Tenis</strong>
-        <span>R$ 120,88</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" />
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
-
-export default Home;
